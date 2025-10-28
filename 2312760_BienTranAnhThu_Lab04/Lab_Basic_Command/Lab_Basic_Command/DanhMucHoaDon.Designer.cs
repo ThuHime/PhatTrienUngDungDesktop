daff7180e523bfa24ffeac009e25d34650e8e3d4
Thurie <2312760@dlu.edu.vn>
2025-10-28 21:53:34 +0700

    Add files via upload

---
+++ 2312760_BienTranAnhThu_Lab04/Lab_Basic_Command/Lab_Basic_Command/DanhMucHoaDon.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Lab_Basic_Command
+{
+    partial class frmDanhMuc
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lsNgayLap = new System.Windows.Forms.ListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dgvCTHD = new System.Windows.Forms.DataGridView();
+            this.FoodName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Quantity = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Amount = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Total = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lblTongTien = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCTHD)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lsNgayLap);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(233, 207);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ngày lập hóa đơn";
+            // 
+            // lsNgayLap
+            // 
+            this.lsNgayLap.FormattingEnabled = true;
+            this.lsNgayLap.Location = new System.Drawing.Point(6, 19);
+            this.lsNgayLap.Name = "lsNgayLap";
+            this.lsNgayLap.Size = new System.Drawing.Size(214, 173);
+            this.lsNgayLap.TabIndex = 0;
+            this.lsNgayLap.SelectedIndexChanged += new System.EventHandler(this.lsNgayLap_SelectedIndexChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dgvCTHD);
+            this.groupBox2.Location = new System.Drawing.Point(251, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(472, 207);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Thông tin chi tiết";
+            // 
+            // dgvCTHD
+            // 
+            this.dgvCTHD.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvCTHD.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCTHD.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.FoodName,
+            this.Quantity,
+            this.Amount,
+            this.Total});
+            this.dgvCTHD.Location = new System.Drawing.Point(6, 19);
+            this.dgvCTHD.Name = "dgvCTHD";
+            this.dgvCTHD.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvCTHD.Size = new System.Drawing.Size(460, 182);
+            this.dgvCTHD.TabIndex = 0;
+            // 
+            // FoodName
+            // 
+            this.FoodName.DataPropertyName = "Name";
+            this.FoodName.HeaderText = "Tên món ăn";
+            this.FoodName.Name = "FoodName";
+            // 
+            // Quantity
+            // 
+            this.Quantity.DataPropertyName = "Quantity";
+            this.Quantity.HeaderText = "Số lượng";
+            this.Quantity.Name = "Quantity";
+            // 
+            // Amount
+            // 
+            this.Amount.DataPropertyName = "Amount";
+            this.Amount.HeaderText = "Đơn giá";
+            this.Amount.Name = "Amount";
+            // 
+            // Total
+            // 
+            this.Total.DataPropertyName = "ThanhTien";
+            this.Total.HeaderText = "Thành tiền";
+            this.Total.Name = "Total";
+            // 
+            // lblTongTien
+            // 
+            this.lblTongTien.AutoSize = true;
+            this.lblTongTien.Location = new System.Drawing.Point(257, 236);
+            this.lblTongTien.Name = "lblTongTien";
+            this.lblTongTien.Size = new System.Drawing.Size(10, 13);
+            this.lblTongTien.TabIndex = 2;
+            this.lblTongTien.Text = ".";
+            // 
+            // frmDanhMuc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(743, 346);
+            this.Controls.Add(this.lblTongTien);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmDanhMuc";
+            this.Text = "Danh mục hóa đơn";
+            this.Load += new System.EventHandler(this.frmDanhMuc_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCTHD)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox lsNgayLap;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dgvCTHD;
+        private System.Windows.Forms.Label lblTongTien;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FoodName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Quantity;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Amount;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Total;
+    }
+}
